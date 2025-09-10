af38af02ed16620b1df23419bcb954f75acf9366
valentinebissani <valentinebissani@gmail.com>
2025-09-10 07:54:11 -0300

    Update jogocsharp.js

--- jogocsharp.js
+++ jogocsharp.js
@@ -43,3 +43,44 @@ document.getElementById('quizForm').addEventListener('submit', function(e){
   resultadoTexto += "</ul><p><strong>Total de acertos:</strong> "+acertos+" de "+total+"</p>";
   document.getElementById("resultado").innerHTML = resultadoTexto;
 });
+
+
+
+
+
+
+
+
+var modo = 0;
+
+function modoEscuro() {
+    if (modo == 0) {
+        document.querySelectorAll('.alterar').forEach(function(element) {
+            element.classList.remove("claro");
+            element.classList.add("escuro");
+        });
+
+        document.querySelectorAll('.alterar2').forEach(function(element) {
+            element.classList.remove("claro");
+            element.classList.add("escuro2");
+        });
+
+        document.getElementById("textmode").innerHTML = "Modo claro";
+        modo = 1;
+    } else {
+        document.querySelectorAll('.alterar').forEach(function(element) {
+            element.classList.remove("escuro");
+            element.classList.add("claro");
+        });
+
+        document.querySelectorAll('.alterar2').forEach(function(element) {
+            element.classList.remove("escuro2");
+            element.classList.add("claro");
+        });
+
+        document.getElementById("textmode").innerHTML = "Modo escuro";
+        modo = 0;
+    }
+}
+
+
